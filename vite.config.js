259070b725be30b012a110c5d35942bb50495c62
jefiarunor <jefia.runor@techmaestrogroup.com>
2024-11-30 16:12:56 +0100

    merge remote change

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target:'http://localhost:3000',
+//         secure:false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'http://localhost:3000',
-        secure:false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://cybersafeapi.onrender.com',
+        // http://localhost:3000
+        secure: false,
+        changeOrigin: true, // Add this for proxy to work properly
       },
     },
   },
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['axios'], // Ensure axios is treated as an external dependency
+    },
+  },
+});
